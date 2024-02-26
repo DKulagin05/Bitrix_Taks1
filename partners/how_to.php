852745ef7b28f51109fb733f5f83008ba1de0d96
DKulagin05 <91366095+DKulagin05@users.noreply.github.com>
2024-02-26 14:51:43 +0500

    Add files via upload

---
+++ partners/how_to.php
@@ -0,0 +1,10 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("keywords", "заявка, партнерский договор");
+$APPLICATION->SetPageProperty("description", "Порядок подачи заявки на партнерство и бланк партнерского договора");
+$APPLICATION->SetTitle("Партнерам");
+?>
+
+    <h2>Как стать партнером</h2>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ user/index.php
@@ -0,0 +1,18 @@
+<?php
+
+define("NEED_AUTH", true);
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+if (isset($_REQUEST["backurl"]) && strlen($_REQUEST["backurl"]) > 0) {
+    LocalRedirect($backurl);
+}
+$APPLICATION->SetTitle("Авторизация");
+?>
+<p>Вы зарегистрированы и успешно авторизовались.</p>
+
+<p><a href="<?=SITE_DIR?>">Вернуться на главную страницу</a></p>
+<?php
+
+// Подключение подвального файла Bitrix
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
+
+?>
